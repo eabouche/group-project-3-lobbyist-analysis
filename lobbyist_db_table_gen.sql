df77536c4666a5128bd49f674cb4c0a54934d7a0
eabouche <eabouche@gmail.com>
2023-02-17 13:45:17 -0600

    organizaing api and dataframe code

--- lobbyist_db_table_gen.sql
+++ lobbyist_db_table_gen.sql
@@ -15,6 +15,32 @@ CREATE TABLE lobbyist_employer_client_rel (
 	year VARCHAR
 );
 
+CREATE TABLE lobbyist_contribution (
+	contribution_id VARCHAR,
+	period_start VARCHAR,
+	period_end VARCHAR,
+	contribution_date VARCHAR,
+	recipient VARCHAR,
+	amount float,
+	lobbyist_id VARCHAR,
+	lobbyist_first_name VARCHAR,
+	lobbyist_last_name VARCHAR,
+	created_date VARCHAR
+);
+
+CREATE TABLE compensation (
+    client_id VARCHAR,
+    client_name VARCHAR,
+    compensation_amount FLOAT,
+    compensation_id VARCHAR,
+    lobbyist_first_name VARCHAR,
+    lobbyist_id VARCHAR,
+    lobbyist_last_name VARCHAR,
+    lobbyist_middle_initial VARCHAR,
+    period_end VARCHAR,
+    period_start VARCHAR
+);
+
 -- Data loaded validation
 Select * From lobbyist_employer_client_rel;
-
+Select * From compensation;
